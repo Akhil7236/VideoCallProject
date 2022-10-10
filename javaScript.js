74ff54e8635a2e2c887b41ed41610478da20d1a0
Akhilesh <akhileshkumarprajapati7706@gmail.com>
2022-10-10 14:01:12 +0530

    sample commit

---
+++ javaScript.js
@@ -0,0 +1,10 @@
+
+function functionCall(){
+    var clickUl=document.querySelector(".ulList");
+    if(clickUl.style.display==="none"){
+        clickUl.style.display="block";
+    }
+    else{
+        clickUl.style.display="none";
+    }
+}
